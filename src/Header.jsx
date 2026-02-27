27d6bb8127ee549654cb202de10ae92457a043af
Rzooro <roronoa.zoro9436@gmail.com>
2026-02-27 17:23:54 +0530

    FinalLogo

--- src/Header.jsx
+++ src/Header.jsx
@@ -34,22 +34,22 @@ function Header() {
       } ${scrolled ? "bg-white/95 backdrop-blur-md shadow-lg" : "bg-white shadow-sm"}`}>
       <div className="py-4 border-b border-gray-100">
         <div className="max-w-7xl mx-auto px-4 md:px-10 flex justify-between items-center">
-          <div className="flex items-center gap-6 group cursor-pointer">
+          <div className="flex items-center gap-2 md:gap-6 group cursor-pointer">
             <img
               src={logo}
               alt="Chanakya University Logo"
-              className="w-24 h-12 md:w-56 md:h-14 object-contain"
+              className="w-32 md:w-56 object-contain"
             />
             <div className="hidden md:block w-px h-10 bg-gray-200" />
             <img
               src={iic}
               alt="IIC Logo"
-              className="w-12 md:w-32 object-contain"
+              className="w-22 md:w-32 object-contain"
             />
           </div>
           <button
             onClick={scrollToRegister}
-            className="bg-[#002855] text-[#C5941C] border border-[#C5941C] px-6 py-2 rounded-sm text-xs font-bold uppercase tracking-widest hover:bg-[#C5941C] hover:text-white transition-all duration-300 shadow-md">
+            className="bg-[#002855] text-[#C5941C] border border-[#C5941C] px-4 py-1 md:px-6 md:py-2 rounded-sm text-xs font-bold uppercase tracking-widest hover:bg-[#C5941C] hover:text-white transition-all duration-300 shadow-md">
             Register Now
           </button>
         </div>
